2170c4e0107fd6c11413a75f610c0a716e7199f6
Jasdeep Kaur <jasdeepkaur1010@gmail.com>
2023-04-28 09:16:23 -0500

    Using functions

---
+++ even.js
@@ -0,0 +1,10 @@
+const isEven = function(num){
+return num%2 === 0;
+}
+const ten = isEven(10);
+const eleven = isEven(11);
+console.log(ten);
+console.log(eleven);
+
+console.log(isEven(9));
+
---
+++ odd.js
@@ -0,0 +1,7 @@
+var isOdd = function(b){
+return b%2!==0;
+}
+
+console.log("3 is odd: " + isOdd(3));
+console.log("8 is odd: " + isOdd(8));
+
