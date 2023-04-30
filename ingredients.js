9ea0abef1c617da85a8772925001d5dd259013d3
Jasdeep Kaur <jasdeepkaur1010@gmail.com>
2023-04-30 00:36:10 -0500

    Ingredients for banana bread

---
+++ ingredients.js
@@ -0,0 +1,12 @@
+const ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+var x = 0;
+while (x < ingredients.length){
+  x++;
+  console.log(ingredients);
+}
+for (var y = 0; y < ingredients.length; y++){
+  console.log(ingredients);
+}
+
+ingredients.reverse();
+console.log(ingredients);
